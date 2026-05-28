6ff0e75b864a8212495aa07c1f93fa8d7b40d056
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-05-27 22:12:41 -0300

    Criando o esqueleto dos endpoints

--- main.go
+++ main.go
@@ -9,5 +9,44 @@ func main() {
 		ctx.String(200, "pong")
 	})
 
+	users := api.Group("/users")
+
+	users.POST("/", func(ctx *gin.Context) {
+	})
+
+	users.GET("/:id", func(ctx *gin.Context) {
+	})
+
+	users.PUT("/:id", func(ctx *gin.Context) {
+	})
+
+	users.DELETE("/:id", func(ctx *gin.Context) {
+	})
+
+	tasks := api.Group("/tasks")
+
+	tasks.POST("/", func(ctx *gin.Context) {
+	})
+
+	tasks.GET("/:id", func(ctx *gin.Context) {
+	})
+
+	tasks.GET("?assignedTo={:id}", func(ctx *gin.Context) {
+	})
+
+	tasks.PUT("/:id", func(ctx *gin.Context) {
+	})
+
+	tasks.DELETE("/:id", func(ctx *gin.Context) {
+	})
+
+	auth := api.Group("/auth")
+
+	auth.POST("/login", func(ctx *gin.Context) {
+	})
+
+	auth.POST("/logout", func(ctx *gin.Context) {
+	})
+
 	api.Run(":8080")
 }
