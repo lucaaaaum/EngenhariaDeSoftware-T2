30ea0864cdae58c7eff6930eaba296d22aac3edf
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-05-28 13:31:01 -0300

    Criando DTOs e serviços na camada de aplicação

---
+++ internal/application/user/service.go
@@ -0,0 +1,56 @@
+package user
+
+import (
+	"context"
+	"errors"
+	"tarefas/internal/domain/user"
+
+	"github.com/google/uuid"
+)
+
+type Service struct {
+	repo user.Repository
+}
+
+func NewService(repo user.Repository) *Service {
+	return &Service{repo: repo}
+}
+
+func (s *Service) GetUserById(ctx context.Context, id uuid.UUID) (*user.User, error) {
+	return s.repo.GetUserById(ctx, id)
+}
+
+func (s *Service) CreateUser(ctx context.Context, command CreateUserCommand) (*user.User, error) {
+	user, err := user.NewUser(command.Name)
+	if err != nil {
+		return nil, errors.Join(errors.New("Failed to create user"), err)
+	}
+
+	s.repo.AddUser(ctx, user)
+
+	return user, nil
+}
+
+func (s *Service) UpdateUser(ctx context.Context, command UpdateUserCommand) error {
+	user, err := s.repo.GetUserById(ctx, command.Id)
+	if err != nil {
+		return errors.Join(errors.New("Failed to get user by id"), err)
+	}
+
+	user.Name = command.Name
+
+	err = s.repo.UpdateUser(ctx, user)
+	if err != nil {
+		err = errors.Join(errors.New("Failed to update user"), err)
+	}
+
+	return err
+}
+
+func (s *Service) DeleteUser(ctx context.Context, command DeleteUserCommand) error {
+	err := s.repo.DeleteUser(ctx, command.Id)
+	if err != nil {
+		err = errors.Join(errors.New("Failed to delete user"), err)
+	}
+	return err
+}
