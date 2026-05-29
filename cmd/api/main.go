98eeb2ad6df4b0243c87918f317417deafb6c3e5
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-05-28 21:04:22 -0300

    Implementando demais endpoints de usuário

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"tarefas/internal/infrastructure/db"
 
 	"github.com/go-fuego/fuego"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 )
 
@@ -35,20 +36,61 @@ func main() {
 
 	users := fuego.Group(server, "/users")
 
-	fuego.Post(users, "/", func(c fuego.ContextNoBody) (string, error) {
-		return "User created", nil
+	fuego.Post(users, "/", func(c fuego.ContextWithBody[user.CreateUserCommand]) (*user.UserDto, error) {
+		cmd, err := c.Body()
+		if err != nil {
+			return nil, errors.Join(errors.New("Failed to parse request body"), err)
+		}
+		createdUser, err := userService.CreateUser(c.Context(), cmd)
+		if err != nil {
+			return nil, errors.Join(errors.New("Failed to create user"), err)
+		}
+
+		return user.NewUserDto(createdUser), nil
 	})
 
-	fuego.Get(users, "/:id", func(c fuego.ContextNoBody) (string, error) {
-		return "user", nil
+	fuego.Get(users, "/{id}", func(c fuego.ContextNoBody) (*user.UserDto, error) {
+		stringId := c.PathParam("id")
+		id, err := uuid.Parse(stringId)
+		if err != nil {
+			return nil, errors.Join(errors.New("Invalid user ID"), err)
+		}
+		userFound, err := userRepo.GetUserById(c.Context(), id)
+		if err != nil {
+			return nil, errors.Join(errors.New("Failed to get user"), err)
+		}
+		return user.NewUserDto(userFound), nil
 	})
 
-	fuego.Put(users, "/:id", func(c fuego.ContextNoBody) (string, error) {
-		return "User updated", nil
+	fuego.Put(users, "/:id", func(c fuego.ContextWithBody[user.UpdateUserCommand]) (any, error) {
+		cmd, err := c.Body()
+		if err != nil {
+			return nil, errors.Join(errors.New("Failed to parse request body"), err)
+		}
+		id, err := uuid.Parse(c.PathParam("id"))
+		if err != nil {
+			return nil, errors.Join(errors.New("Invalid user ID"), err)
+		}
+		cmd.Id = id
+		err = userService.UpdateUser(c.Context(), cmd)
+		if err != nil {
+			return nil, errors.Join(errors.New("Failed to update user"), err)
+		}
+		c.SetStatus(204)
+		return nil, nil
 	})
 
-	fuego.Delete(users, "/:id", func(c fuego.ContextNoBody) (string, error) {
-		return "User deleted", nil
+	fuego.Delete(users, "/:id", func(c fuego.ContextNoBody) (any, error) {
+		id, err := uuid.Parse(c.PathParam("id"))
+		if err != nil {
+			return nil, errors.Join(errors.New("Invalid user ID"), err)
+		}
+		err = userService.DeleteUser(c.Context(), id)
+		if err != nil {
+			return nil, errors.Join(errors.New("Failed to delete user"), err)
+		}
+		c.SetStatus(204)
+		return nil, nil
 	})
 
 	tasks := fuego.Group(server, "/tasks")
